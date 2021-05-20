ad735e26f24d2d31053732c04f097ebf4c7157fe
huangshiwei37 <huangshiwei37@gmail.com>
2021-05-20 15:52:26 -0700

    add join example

--- common/src/main/java/multithreading/WaitAThreadToFinish.java
+++ common/src/main/java/multithreading/WaitAThreadToFinish.java
@@ -7,17 +7,23 @@ public class WaitAThreadToFinish {
 
     public static void main(String[] arg){
         Thread t1 = new Thread(() -> {
-            System.out.println("Thread 1: start");
+            System.out.println("Thread 1: start downloadImage...");
             try {
                 Thread.sleep(2000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
-            System.out.println("Thread 1:: end");
+            System.out.println("Thread 1:: end downloadImage");
+        });
+        Thread t2 = new Thread(() -> {
+            System.out.println("Thread 2: start processImage...");
+            System.out.println("Thread 2:: end processImage");
         });
         t1.start();
+        t2.start();
         try {
             t1.join();
+            t2.join();
         }catch (InterruptedException e){
             e.printStackTrace();
         }
