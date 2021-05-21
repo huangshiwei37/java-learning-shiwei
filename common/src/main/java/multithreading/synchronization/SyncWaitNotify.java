f184cd84224ec9c49215236aaf86d62b1d03f22d
huangshiwei37 <huangshiwei37@gmail.com>
2021-05-21 15:41:26 -0700

    add wait and notify

---
+++ common/src/main/java/multithreading/synchronization/SyncWaitNotify.java
@@ -0,0 +1,55 @@
+package multithreading.synchronization;
+
+/**
+ * @author huangshiwei on 2021-05-21
+ */
+public class SyncWaitNotify {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.produce();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.consume();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Processor {
+    public void produce() throws InterruptedException{
+        synchronized (this){
+            System.out.println("produce() is running ...");
+            wait(); // It releases the intrinsic lock after calling wait() and it will re-obtain the intrinsic lock after another thread notifies it
+            System.out.println("produce() ends");
+        }
+    }
+
+    public void consume() throws InterruptedException{
+        synchronized (this){
+            System.out.println("consume() is running ...");
+            notify();
+            Thread.sleep(2000);
+            System.out.println("consume() ends");
+        } // release lock after executing the block only
+    }
+
+}
+
